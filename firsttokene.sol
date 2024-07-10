e00371772b70d44c958876f98c0bc79365fb1850
Adhiraj Kumar <adhirajkumar.178@gmail.com>
2024-07-10 22:32:55 +0530

    project ETH proof

--- firsttokene.sol
+++ firsttokene.sol
@@ -1,46 +1,28 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.26;
+pragma solidity >=0.8.9;
 
-/*
-       REQUIREMENTS
-    1. Your contract will have public variables that store the details about your coin (Token Name, Token Abbrv., Total Supply)
-    2. Your contract will have a mapping of addresses to balances (address => uint)
-    3. You will have a mint function that takes two parameters: an address and a value. 
-       The function then increases the total supply by that number and increases the balance 
-       of the “sender” address by that amount
-    4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
-       It will take an address and value just like the mint functions. It will then deduct the value from the total supply 
-       and from the balance of the “sender”.
-    5. Lastly, your burn function should have conditionals to make sure the balance of "sender" is greater than or equal 
-       to the amount that is supposed to be burned.
-*/
+contract AdhirajToken {
 
-contract MyToken {
+    // Public variables to store token details
+    string public tokenName = "AdhirajToken";
+    string public tokenAbbrv = "ATK";
+    uint public totalSupply = 1000;
 
-    // public variables here
+    // Mapping from addresses to their balances
+    mapping(address => uint) public balances;
 
-    string public tokenName="Adhiraj Kumar";
-    string public tokenAbbre="Amit";
-    uint public totalSupp=3000;
-
-// mapping variable here
-
-    mapping (address=>uint) public balance;
-    
-    // mint function
-    
-    function mint(address _address, uint _value) public {
-        totalSupp+= _value;
-        balance[_address]+= _value;
+    // Mint function to create new tokens
+    function mint(address _to, uint _value) public {
+        totalSupply += _value;
+        balances[_to] += _value;
     }
 
-    // burn function
-
-    function burn(address _address, uint _value) public{
-        if(balance[_address]>=_value){
-            totalSupp-= _value;
-            balance[_address]-=_value;
-}
-}
+    // Burn function to destroy tokens
+    function burn(address _address, uint _value) public {
+        if (balances[_address] >= _value){
+            totalSupply -= _value;
+            balances[_address] -= _value;
+        }
+    }
 
 }
